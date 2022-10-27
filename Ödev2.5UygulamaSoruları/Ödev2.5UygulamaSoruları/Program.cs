7a2de3aab3c542ebe0df0c109f1ff5aea86368ab
hilalldavas <hilalldavas@gmail.com>
2022-10-27 13:23:02 +0300

    Odev2.5UygSoru

---
+++ Ödev2.5UygulamaSoruları/Ödev2.5UygulamaSoruları/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ödev2._5UygulamaSoruları
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { Console.Write("A Değerini Giriniz = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("B Değerini Giriniz = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("C Değerini Giriniz = ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            double d = (b * b) - (4 * a * c);
+            if (d > 0)
+            {
+                double x1 = ((-b - Math.Sqrt(d)) / (2 * a));
+                double x2 = ((-b + Math.Sqrt(d)) / (2 * a));
+                Console.WriteLine("Denklemin İki Kökü Vardır = ");
+                Console.WriteLine("x1 =" + x1);
+                Console.WriteLine("x2 =" + x2);
+            }
+            else if (d == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine("Denklemin Bir Kökü Vardır = ");
+                Console.WriteLine("x =" + x);
+            }
+            else
+            {
+                Console.Write("Sanal Kök Vardır");
+            }
+            Console.ReadKey();
+        }
+    }
+}
